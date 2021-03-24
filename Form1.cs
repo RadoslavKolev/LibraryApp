0dd55dbeff3876e7498bedece19f55c9c0ed0af2
radokolev99 <radoslav.kolev@abv.bg>
2021-03-25 00:00:08 +0200

    Repaired something

--- Form1.cs
+++ Form1.cs
@@ -25,7 +25,7 @@ namespace LibraryApp
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Hello World!");
-            MessageBox.Show("Bye World");
+            MessageBox.Show("Goodbye World");
             MessageBox.Show("By Rado & Niki");
         }
     }
