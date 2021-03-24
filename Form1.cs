6468dd369e5285fdcb6da827038bad9e32da0e7d
nikito-error <nisanx@abv.bg>
2021-03-24 23:37:26 +0200

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -25,6 +25,7 @@ namespace LibraryApp
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Hello World!");
+            MessageBox.Show("Bye World");
         }
     }
 }
